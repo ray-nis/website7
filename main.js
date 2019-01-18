1f7a58d9b0fb0d17209536de2e6a40e26740ef60
ray-nis <reirei256@gmail.com>
2019-01-18 10:16:16 +0100

    uh

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 $("#menu-bars").click(function () {
     $(".nav").slideToggle("fast", "linear");
 });
+if (screen.width > 1000) {
+    document.querySelector("#animation1").classList.add("scale-up-center");
+    document.querySelector("#animation2").classList.add("scale-up-center");
+}
