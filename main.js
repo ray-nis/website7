3167d0b5ed323ef831bdf645f8baf6225889f2af
ray-nis <reirei256@gmail.com>
2019-01-18 12:13:40 +0100

    navbar

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 $("#menu-bars").click(function () {
     $(".nav").slideToggle("fast", "linear");
 });
-if (screen.width > 1000) {
-    document.querySelector("#animation1").classList.add("scale-up-center");
-    document.querySelector("#animation2").classList.add("scale-up-center");
-}
