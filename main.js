2c48748099af9487696dd284b4e990c89db8cad7
ray-nis <reirei256@gmail.com>
2019-01-19 07:01:11 +0100

    js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-$("#menu-bars").click(function () {
+function menuSlide() {
     $(".nav").slideToggle("fast", "linear");
-});
+}
+$("#menu-bars").click(menuSlide);
+
+if (screen.width < 1000) {
+    $("#m1").click(menuSlide);
+    $("#m2").click(menuSlide);
+    $("#m3").click(menuSlide);
+    $("#m4").click(menuSlide);
+    $("#m5").click(menuSlide);
+    $("#m6").click(menuSlide);
+}
