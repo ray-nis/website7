b8f0b23fb1a8aca22d1ef3da956db5a37a35da5a
ray-nis <reirei256@gmail.com>
2019-01-20 09:19:35 +0100

    animation

--- main.js
+++ main.js
@@ -23,11 +23,30 @@ function isScrolledIntoView(el) {
 
 let aboutImg = document.querySelector("#about-img");
 let aboutText = document.querySelector(".about-text");
+let services = document.querySelectorAll(".service");
 window.onscroll = () => {
     if (isScrolledIntoView(aboutImg)) {
         aboutImg.classList.add("scale-up-center");
     }
     if (isScrolledIntoView(aboutText)) {
         aboutText.classList.add("slide-in-top");
+    }
+    if (isScrolledIntoView(services[0])) {
+        services[0].classList.add("puff-in-top");
+    }
+    if (isScrolledIntoView(services[1])) {
+        services[1].classList.add("puff-in-top");
+    }
+    if (isScrolledIntoView(services[2])) {
+        services[2].classList.add("puff-in-top");
+    }
+    if (isScrolledIntoView(services[3])) {
+        services[3].classList.add("puff-in-top");
+    }
+    if (isScrolledIntoView(services[4])) {
+        services[4].classList.add("puff-in-top");
+    }
+    if (isScrolledIntoView(services[5])) {
+        services[5].classList.add("puff-in-top");
     }
 }
